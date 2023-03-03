a05a216638950bec49fe828d7dcd4bb7d4a367ba
matelko <mateusz280798@gmail.com>
2023-03-03 23:04:54 +0100

    Keeping track of the endpoints

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -3,11 +3,11 @@ using Movies.Api.Mapping;
 using Movies.Application.Models;
 using Movies.Application.Repositories;
 using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
 
 namespace Movies.Api.Controllers;
 
 [ApiController]
-[Route("api")]
 public class MoviesController : ControllerBase
 {
     private readonly IMovieRepository _movieRepository;
@@ -17,11 +17,32 @@ public class MoviesController : ControllerBase
         _movieRepository = movieRepository;
     }
 
-    [HttpPost("movies")]
+    [HttpPost(ApiEndpoints.Movies.Create)]
     public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
     {
         Movie movie = request.MaptoMovie();
         bool result = await _movieRepository.CreateAsync(movie);
         return Created($"/api/movies/{movie.Id}", movie);
+    }
+
+    [HttpGet(ApiEndpoints.Movies.Get)]
+    public async Task<IActionResult> Get([FromRoute] Guid id)
+    {
+        Movie? movie = await _movieRepository.GetByIdAsync(id);
+        if (movie is null)
+        {
+            return NotFound();
+        }
+
+        MovieResponse response = movie.MapToResponse();
+        return Ok(response);
+    }
+
+    [HttpGet(ApiEndpoints.Movies.GetAll)]
+    public async Task<IActionResult> GetAll()
+    {
+        IEnumerable<Movie> movies = await _movieRepository.GetAllAsync();
+        MoviesResponse response = movies.MapToResponse();
+        return Ok(response);
     }
 }
