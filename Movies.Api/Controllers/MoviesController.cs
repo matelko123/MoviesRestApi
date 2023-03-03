a5da206678207370c22bd06c4985dcdd38407dee
matelko <mateusz280798@gmail.com>
2023-03-03 22:52:18 +0100

    Implementing movie creation

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Models;
 using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
 
 namespace Movies.Api.Controllers;
 
 [ApiController]
+[Route("api")]
 public class MoviesController : ControllerBase
 {
     private readonly IMovieRepository _movieRepository;
@@ -12,6 +15,19 @@ public class MoviesController : ControllerBase
     {
         _movieRepository = movieRepository;
     }
-    
-    
+
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        Movie movie = new Movie
+        {
+            Id = Guid.NewGuid(),
+            Title = request.Title,
+            YearOfRelease = request.YearOfRelease,
+            Genres = request.Genres.ToList()
+        };
+
+        bool result = await _movieRepository.CreateAsync(movie);
+        return Created($"/api/movies/{movie.Id}", movie);
+    }
 }
